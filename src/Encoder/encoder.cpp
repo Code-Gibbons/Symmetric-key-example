ea785020d0fb9befcf3c6d618b5a54ea4f0ae31c
Alec <the_didz@yahoo.com>
2023-07-28 14:52:49 -0400

    oop is starting to come back to me

--- src/Encoder/encoder.cpp
+++ src/Encoder/encoder.cpp
@@ -1,19 +1,104 @@
-#include <iostream> //
+#include <iostream>
+#include <readline/readline.h>
+
 #include "encoder.h"
 
-namespace encoder
+const char keyArgChar = 'k';
+const char msgArgChar = 'm';
+const char helpArgChar = 'h';
+const char flagChar =  '-';
+
+//******************************************************************************
+// Constructors
+//******************************************************************************
+encoder::Encoder::Encoder(std::string userKey, std::string userMsg)
 {
-    std::string GetUserKey()
-    {
-        std::string inputKey;
-        std::getline(std::cin, inputKey);
-        return inputKey;
-    }
+    encoder::Encoder::userKey = userKey;
+    encoder::Encoder::plainTextMsg = userMsg;
+    encoder::Encoder::cipherText = ""; // Empty cipher text until processed
+}
+
+//******************************************************************************
+// Setters
+//******************************************************************************
+
+void encoder::Encoder::SetPlainTextMsg(std::string userMsg)
+{
+    encoder::Encoder::plainTextMsg = userMsg;
+}
+
+void encoder::Encoder::SetUserKey(std::string userKey)
+{
+    encoder::Encoder::userKey = userKey;
+}
+
+void encoder::Encoder::SetCipherText(std::string cipherText)
+{
+    encoder::Encoder::cipherText = cipherText;
+}
+
+//******************************************************************************
+// Getters
+//******************************************************************************
+std::string encoder::Encoder::GetUserKey()
+{
+    return encoder::Encoder::userKey;
+}
+
+std::string encoder::Encoder::GetPlainTextMsg()
+{
+    return encoder::Encoder::plainTextMsg;
+}
+
+std::string encoder::Encoder::GetCipherText()
+{
+    return encoder::Encoder::cipherText;
 }
 
 
-int main()
+int main(int argc, char **argv)
 {
-    std::string userInput = encoder::GetUserKey();
-    std::cout << "User input was: " << userInput << std::endl;
+    std::string passKeyArg;
+    std::string messageArg;
+
+    // Don't forget to start at one so we skip program name
+    for(int i = 1; i <argc; ++i)
+    {
+        // Look for the flag and check if it's a supported one
+        if(argv[i][0] == flagChar)
+        {
+            // Don't overflow past max inputs
+            if( i + 1 < argc)
+            {
+                if(argv[i][1] == 'k')
+                {
+                    passKeyArg = argv[i +1];
+                    i++;
+                }
+                else if(argv[i][1] == 'm')
+                {
+                    messageArg = argv[i+1];
+                    i++;
+                }
+                else
+                {
+                    std::cout << "Option: " << argv[i] << " requires a value." << std::endl;
+                }
+            }
+            else
+            {
+                std::cout << "Unsupported argument entered: " << argv[i] << std::endl;
+            }
+
+        }
+        else
+        {
+            std::cout << "Error.\n Flag provided: " << argv[i]
+            << " is not a supported flag.\n Please input a supported flag." << std::endl;
+        }
+
+
+
+    }
+
 }
--- src/Encoder/encoder.h
+++ src/Encoder/encoder.h
@@ -5,7 +5,27 @@
 
 namespace encoder
 {
+    class Encoder
+    {
+    private:
+    std::string userKey;
+    std::string plainTextMsg;
+    std::string cipherText;
+
+    public:
+    // Constructor
+    Encoder(std::string userKey, std::string userMsg);
+
+    // Setters
+    void SetUserKey(std::string userKey);
+    void SetPlainTextMsg(std::string userMsg);
+    void SetCipherText(std::string cipherMsg);
+
+    // Getters
     std::string GetUserKey();
+    std::string GetPlainTextMsg();
+    std::string GetCipherText();
+    };
 }
 
 #endif // ENCODER_H
