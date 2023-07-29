f89fec03f783ae381b1f7fb28b8a37306dde48c8
Alec <the_didz@yahoo.com>
2023-07-29 17:53:12 -0400

    I need to stop saving generic snippits or learn to read user buildDir not .

--- cmake-build.ps1
+++ cmake-build.ps1
@@ -1,13 +1,25 @@
-param (
-    [string]$target = "encode"
-)
+# Get the script's directory and set it as the working directory
+$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Path
+Set-Location $scriptPath
+
+$target = $args[0]
 
 # Set the build directory based on the target argument
 $buildDir = if ($target -eq "decode") {
     "build_decode"
 }
-else {
+elseif ($target -eq "encode") {
     "build_encode"
+}
+else {
+    Write-Host("No valid build target provided. Exiting build script. Please pass either encode or decode.")
+    exit
+}
+
+Write-Host "Checking for old build."
+if (Test-Path -Path $buildDir -PathType Container) {
+    Remove-Item -Path $buildDir -Force -Recurse
+    Write-Host "Deleted prior build run."
 }
 
 # Create the build directory if it does not exist
@@ -15,14 +27,31 @@ if (-Not (Test-Path $buildDir)) {
     New-Item -ItemType Directory -Path $buildDir | Out-Null
 }
 
-# Change to the build directory
-Set-Location $buildDir
+Set-Location $scriptPath
+Write-Host "Current directory: $(Get-Location)"
 
-# Run the CMake command to configure and generate the build files
-cmake .. -DCMAKE_BUILD_TYPE=Release -DTARGET_TYPE=$target
 
-# Build the project
-cmake --build .
+# Call CMake build with MinGW
+$cmakeCommand = if ($target -eq "decode") {
+    "cmake -S . -B $buildDir -G ""MinGW Makefiles"" -DTARGET_TYPE=decode"
+}
+else {
+    "cmake -S . -B $buildDir -G ""MinGW Makefiles"" -DTARGET_TYPE=encode"
+}
 
-# Change back to the original directory
-Set-Location ..
+# $cmakeCommand = "cmake -S . -B $buildDir -G ""MinGW Makefiles"" -DTARGET_TYPE=encode"
+Write-Host "Generate makefiles for ming32-make."
+Invoke-Expression $cmakeCommand
+
+# Switch dirs
+Write-Host "Switch to makefile dir."
+Push-Location -Path $buildDir
+Write-Host($buildDir)
+
+# Call Makefile
+$makeCommand = "mingw32-make"
+Write-Host "Running makefile."
+Invoke-Expression $makeCommand
+
+# Go back up
+Pop-Location
